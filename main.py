04b634b67264c2deeb91e4f06e21e02f5c860060
Sapfir0 <sapfir999999@yandex.ru>
2021-03-16 19:24:36 +0300

    Правки

--- main.py
+++ main.py
@@ -1,35 +1,95 @@
 import matplotlib.pyplot as plt
 
-parallelMutex = [ 0.28427, 0.289129, 0.323148,  0.355737, 0.332423, 0.347704]
-atomic  = [0.243361, 0.216916, 0.171087, 0.191297, 0.192129, 0.20461]
-shared  = [0.26066, 0.229732, 0.232619, 0.26152, 0.247485, 0.327568]
-threads = [2,3,4,5, 6, 7]
-plt.xlabel('threads')
+raw = """serial 1 100000 0.0181084
+atomic 2 100000 0.0286803
+atomic 3 100000 0.0149497
+atomic 4 100000 0.0177285
+atomic 5 100000 0.0179318
+atomic 6 100000 0.0161278
+atomic 7 100000 0.018824
+parallelMutex 2 100000 0.0165309
+parallelMutex 3 100000 0.0199195
+parallelMutex 4 100000 0.0250316
+parallelMutex 5 100000 0.0320693
+parallelMutex 6 100000 0.0318917
+parallelMutex 7 100000 0.0352808
+sharedVariables 2 100000 0.0122813
+sharedVariables 3 100000 0.0128984
+sharedVariables 4 100000 0.0138137
+sharedVariables 5 100000 0.0183395
+sharedVariables 6 100000 0.0190186
+sharedVariables 7 100000 0.0174331
+serial 1 1000000 0.394883
+atomic 2 1000000 0.255585
+atomic 3 1000000 0.202101
+atomic 4 1000000 0.196983
+atomic 5 1000000 0.262815
+atomic 6 1000000 0.245374
+atomic 7 1000000 0.196857
+parallelMutex 2 1000000 0.253629
+parallelMutex 3 1000000 0.291546
+parallelMutex 4 1000000 0.307338
+parallelMutex 5 1000000 0.330139
+parallelMutex 6 1000000 0.373339
+parallelMutex 7 1000000 0.374449
+sharedVariables 2 1000000 0.194899
+sharedVariables 3 1000000 0.18292
+sharedVariables 4 1000000 0.149915
+sharedVariables 5 1000000 0.165216
+sharedVariables 6 1000000 0.175834
+sharedVariables 7 1000000 0.202868
+serial 1 10000000 4.64155
+atomic 2 10000000 3.0792
+atomic 3 10000000 2.85799
+atomic 4 10000000 4.33126
+atomic 5 10000000 4.71289
+atomic 6 10000000 3.13432
+atomic 7 10000000 2.97651
+parallelMutex 2 10000000 3.75005
+parallelMutex 3 10000000 4.0485
+parallelMutex 4 10000000 4.50492
+parallelMutex 5 10000000 4.33122
+parallelMutex 6 10000000 4.32856
+parallelMutex 7 10000000 4.71381
+sharedVariables 2 10000000 3.43019
+sharedVariables 3 10000000 3.01991
+sharedVariables 4 10000000 3.44454
+sharedVariables 5 10000000 2.86772
+sharedVariables 6 10000000 2.93454
+sharedVariables 7 10000000 3.13343
+"""
+
+rows = raw.split("\n")
+
+for row in rows:
+    [name, threads, size, time] = row.split(" ")
+    plt.plot(size, f"{name} {threads} {size}")
+
+
+plt.xlabel('size')
 plt.ylabel('time')
-plt.plot(threads, parallelMutex, )
-plt.plot(threads, atomic)
-plt.plot(threads, shared)
-plt.legend(["mutex", "atomic", "shared"], loc=2)
+
+plt.legend(rows, loc=2)
 
 plt.show()
 
 # parallelMutex2 0.365488
-# shared variables2 0.292728
+# sharedVariables2 0.292728
 # atomic2 0.234913
 # parallelMutex3 0.28454
-# shared variables3 0.222694
+# sharedVariables3 0.222694
 # atomic3 0.188915
 # parallelMutex4 0.382962
-# shared variables4 
+# sharedVariables4 
 # atomic4 0.177593
 # parallelMutex5 
-# shared variables5 0.284384
+# sharedVariables5 0.284384
 # atomic5 0.189578
 # parallelMutex6 0.328889
-# shared variables6 
+# sharedVariables6 
 # atomic6 
 # parallelMutex7 0.344558
-# shared variables7 
+# sharedVariables7 
 # atomic7 
 
 
--- parallel2/parallel2.cpp
+++ parallel2/parallel2.cpp
@@ -19,32 +19,37 @@ using namespace std;
 
 int main() {
     int N = 100000;
-    int MaxN = 100000000;
+    int MaxN = 10000000;
     const int maxThreadsCount = 8;
-    vector<int> list0(randomVector(N));
+    //vector<int> list0(randomVector(N));
 
-    cout << "serial ";
-    auto serial = measureTime(uniqueCounter<int>)(list0);
 
     map<string, function<int(vector<int>, int)>> types = {
         {"parallelMutex", uniqueCounterMutex<int>},
-        {"shared variables", uniqueCounterSharedVariables<int>},
+        {"sharedVariables", uniqueCounterSharedVariables<int>},
         {"atomic", uniqueCounterAtomic<int>}
     };
     
 
-    for (auto type : types) {
-        for (int size = N; size < MaxN; size *= 10) {
+    for (int size = N; size < MaxN; size *= 2) {
+        auto currentList = randomVector(size);
+        cout << "serial 1 " << size << " ";
+        auto serial = measureTime(uniqueCounter<int>)(currentList);
+
+        for (auto type : types) {
             for (int threadsCount = 2; threadsCount < maxThreadsCount; threadsCount++) {
-                cout << type.first << threadsCount << " ";
+                cout << type.first << " " << threadsCount << " ";
                 int result = 0;
                 double elapsed = 0;
                 cout << size << " ";
-                tie(result, elapsed) = checkTime(type.second, list0, threadsCount);
+                tie(result, elapsed) = checkTime(type.second, currentList, threadsCount);
                 testIsEqualLength(serial, result);
             }
         }
     }
+
+
+
 
     return 0;
 }
